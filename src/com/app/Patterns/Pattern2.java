a6cfd4a618de439e746930df50d118f9cb7eb589
priyamendke <prmendke@gmail.com>
2019-04-16 12:35:04 +0530

    just now change

--- src/com/app/Patterns/Pattern2.java
+++ src/com/app/Patterns/Pattern2.java
@@ -3,15 +3,15 @@ package com.app.Patterns;
 public class Pattern2 {
 
 	public static void main(String[] args) {
-		for(int i=0;i<=4; i++) {
-			
-			for(int j=4; j>=i; j--) {
-				
+		for (int i = 0; i <= 4; i++) {
+
+			for (int j = 4; j >= i; j--) {
+
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-
+		System.out.println("golu");
 	}
 
 }
